e67b1f443b074a79c2c7875f7037a25cd3769a85
Zacarías <zacarias@wichipu.com>
2025-03-08 01:44:29 +0100

    test: Improved Work and Site tests

---
+++ internal/domain/resource.go
@@ -0,0 +1,26 @@
+package domain
+
+import "errors"
+
+type Resource struct {
+	URL      string `json:"url"`
+	MIMEType string `json:"mime_type"`
+	Referer  string `json:"referer"`
+	Content  string `json:"content"`
+}
+
+func NewResource(URL string, MIMEType string, referer string, content string) (*Resource, error) {
+	if URL == "" {
+		return nil, errors.New("error.resource.url_is_empty")
+	}
+	if MIMEType == "" {
+		return nil, errors.New("error.resource.mime_type_is_empty")
+	}
+	if content == "" {
+		return nil, errors.New("error.resource.content_is_empty")
+	}
+	if referer == "" {
+		return nil, errors.New("error.resource.referer_is_empty")
+	}
+	return &Resource{URL: URL, MIMEType: MIMEType, Referer: referer, Content: content}, nil
+}
---
+++ internal/domain/resource_test.go
@@ -0,0 +1,54 @@
+package domain
+
+import "testing"
+
+func TestResource_NewResource(t *testing.T) {
+	t.Run("Should return a error when URL is empty", func(t *testing.T) {
+		URL := ""
+		MIMEType := "text/html"
+		referer := "http://dominio.com/referer"
+		content := "test"
+		resource, err := NewResource(URL, MIMEType, referer, content)
+		if err == nil {
+			t.Errorf("Expected error when url is empty")
+		}
+		if err.Error() != "error.resource.url_is_empty" {
+			t.Errorf("Expected 'error.resource.url_is_empty' error, got %v", err)
+		}
+		if resource != nil {
+			t.Errorf("Expected no resource, got %v", resource)
+		}
+	})
+	t.Run("Should return a error when MIME type is empety", func(t *testing.T) {
+		URL := "http://dominio.com"
+		MIMEType := ""
+		referer := "http://dominio.com/referer"
+		content := "test"
+		resource, err := NewResource(URL, MIMEType, referer, content)
+		if err == nil {
+			t.Error("Expected error when type is empty")
+		}
+		if err.Error() != "error.resource.mime_type_is_empty" {
+			t.Errorf("Expected 'error.resource.mime_type_is_empty' error, got %v", err)
+		}
+		if resource != nil {
+			t.Errorf("Expected no resource, %v got", resource)
+		}
+	})
+	t.Run("Should return a error when referer is empety", func(t *testing.T) {
+		URL := "http://dominio.com"
+		MIMEType := "text/html"
+		referer := ""
+		content := "test"
+		resource, err := NewResource(URL, MIMEType, referer, content)
+		if err == nil {
+			t.Error("Expected error when referer is empty")
+		}
+		if err.Error() != "error.resource.referer_is_empty" {
+			t.Errorf("Expected 'error.resource.referer_is_empty' error, got %v", err)
+		}
+		if resource != nil {
+			t.Errorf("Expected no resource, %v got", resource)
+		}
+	})
+}
